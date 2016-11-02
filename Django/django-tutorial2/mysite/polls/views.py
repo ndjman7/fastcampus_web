c4091c17121200b3edaf1fca592c4cd94d2014f9
Pando <ndjman7@gmail.com>
2016-11-02 16:35:53 +0900

    Function view -> Class view

--- Django/django-tutorial2/mysite/polls/views.py
+++ Django/django-tutorial2/mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
-from django.http import HttpResponse ,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 from polls.models import Question, Choice
 
@@ -23,8 +24,8 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -36,3 +37,21 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
