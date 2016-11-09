880932c4118d4908bf1820cf52d27fab1436dc1a
Pando <ndjman7@gmail.com>
2016-11-09 19:58:40 +0900

    RESTFul Framework chapt01

--- Django/tutorial/tutorial/tutorial/settings.py
+++ Django/tutorial/tutorial/tutorial/settings.py
@@ -37,6 +37,8 @@ REST_FRAMEWORK = {
 
 INSTALLED_APPS = [
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
